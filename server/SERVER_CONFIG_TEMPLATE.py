9cd97a5469e92e29156db7bad109e35c3c5da9de
halal mode <87722214+Bosanac515@users.noreply.github.com>
2024-04-17 14:08:52 -0500

    Update SERVER_CONFIG_TEMPLATE.py
    changed url

--- server/SERVER_CONFIG_TEMPLATE.py
+++ server/SERVER_CONFIG_TEMPLATE.py
@@ -2,7 +2,7 @@
 # This is where you configure your server credentials for connecting to the publication server
 # Hardcode things here if you want :)
 
-MQTT_SERVER = "b0d11619bad64381af076f147cf1cb7c.s1.eu.hivemq.cloud"#input("Enter MQTT server address: ")
+MQTT_SERVER = "ae5fc2f4895f4e748c883406c167b1fe.s1.eu.hivemq.cloud"#input("Enter MQTT server address: ")
 MQTT_PORT = 8883  # Typically, the port is 1883 for MQTT
 MQTT_CLIENT_ID =  input("Enter MQTT client ID: ")
 MQTT_USERNAME = input("Enter MQTT username: ")
